f10d6abc10dcf515892161f72e8a684a16fb2da5
BenjaminAlamu <oluwaseunalamu@gmail.com>
2018-10-19 01:48:18 +0100

    Made changes to the models

--- api/models/Application.js
+++ api/models/Application.js
@@ -8,7 +8,65 @@
 module.exports = {
 
   attributes: {
-
+    //(firstname, lastname, school, companyAccept, studentAccept,companyID)
+    firstName: {
+      type: 'string',
+      required: true,
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+    },
+    school: {
+      type: 'string',
+      required: true,
+    },
+    companyAccept: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    studentAccept: {
+      type: 'boolean',
+      defaultsTo: "none"
+    },
+    companyReject: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    studentReject: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    studentID: {
+      type: 'string',
+      required: true
+    },
+    companyID: {
+      type: 'string',
+      required: true
+    },
+  },
+  validationMessages: {
+    firstName: {
+        string: 'First Name is required',
+        required: 'First Name is required',
+    },
+    lastName: {
+      string: 'Last Name is required',
+      required: 'Last Name is required',
+    },
+    school: {
+      string: 'School Name is required',
+      required: 'School Name is required'
+    },
+    studentID: {
+      string: 'Student ID missing',
+      required: 'Student ID must be passed with request'
+    },
+    companyID: {
+      string: 'Company ID missing',
+      required: 'Company ID must be passed with request'
+    }
   }
 };
 
