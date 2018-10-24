d70ee31ca6426f65e4216abab29bb0ebd1380806
BenjaminAlamu <oluwaseunalamu@gmail.com>
2018-10-24 17:22:11 +0100

    Create the create report controller

--- api/controllers/ReportController.js
+++ api/controllers/ReportController.js
@@ -6,6 +6,23 @@
  */
 
 module.exports = {
-	
+  create: async function(req, res) {
+    try {
+      let payload = req.body;
+      const data = await Report.create(payload);
+      return res.send({
+        status: 200,
+        success: true,
+        data
+      });
+    } catch (err) {
+      console.log(err.message);
+      res.send({
+        status: 400,
+        success: false,
+        message: err.message,
+        err
+      });
+    }
+  }
 };
-
--- api/models/Report.js
+++ api/models/Report.js
@@ -6,9 +6,79 @@
  */
 
 module.exports = {
-
   attributes: {
-
+    student: {
+      model: "student",
+      required: true
+    },
+    monday: {
+      type: "string",
+      required: true
+    },
+    tuesday: {
+      type: "string",
+      required: true
+    },
+    wednesday: {
+      type: "string",
+      required: true
+    },
+    thursday: {
+      type: "string",
+      required: true
+    },
+    friday: {
+      type: "string",
+      required: true
+    },
+    weeklySummary: {
+      type: "string",
+      required: true
+    },
+    companyApprove: {
+      type: "boolean",
+      defaultsTo: false
+    },
+    staffApprove: {
+      type: "boolean",
+      defaultsTo: false
+    },
+    beginDate: {
+      type: "datetime",
+      required: true
+    },
+    endDate: {
+      type: "datetime",
+      required: true
+    }
+  },
+  validationMessages: {
+    student: {
+      required: "Student is required"
+    },
+    monday: {
+      required: "You need to enter what you did on Monday"
+    },
+    tuesday: {
+      required: "You need to enter what you did on Tuesday"
+    },
+    wednesday: {
+      required: "You need to enter what you did on Wednesday"
+    },
+    thursday: {
+      required: "You need to enter what you did on Thursday"
+    },
+    friday: {
+      required: "You need to enter what you did on Friday"
+    },
+    weeklySummary: {
+      required: "You need to enter your weekly Summary"
+    },
+    beginDate: {
+      required: "Please a valid date"
+    },
+    endDate: {
+      required: "Please a valid date"
+    }
   }
 };
-
